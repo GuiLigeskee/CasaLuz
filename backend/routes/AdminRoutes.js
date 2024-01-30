dfe0d0232563d8beefd0b9b4a91bc1128e73e2f6
Guilherme Ligeski Saldanha <112131340+GuiLigeskee@users.noreply.github.com>
2024-01-30 15:48:24 -0300

    Atualizações dia 30/01/2024
    --Criação dos middlewares de validação;
    --Rotas de admin funcionando como o esperado;

--- backend/routes/AdminRoutes.js
+++ backend/routes/AdminRoutes.js
@@ -11,13 +11,23 @@ const {
 } = require("../controllers/AdminController");
 
 // Middlewares
+const validate = require("../middlewares/handleValidation");
+const {
+  adminCreateValidation,
+  loginValidation,
+  adminUpdateValidation,
+} = require("../middlewares/adminValidation");
+const authGuard = require("../middlewares/authGuard");
 
 // Routes
-router.post("/register", register);
-router.get("/profile", getCurrentAdmin);
-router.post("/login", login);
+router.post("/register", authGuard, adminCreateValidation(), validate, register);
+router.get("/profile", authGuard, validate, getCurrentAdmin);
+router.post("/login",loginValidation(), validate, login);
 router.put(
   "/",
+  authGuard,
+  adminUpdateValidation(),
+  validate,
   update
 );
 router.get("/:id", getAdminById);
