6e62698b4fc906ca883101f3470367a1c1d2df73
Guilherme Ligeski Saldanha <112131340+GuiLigeskee@users.noreply.github.com>
2024-03-11 15:21:37 -0300

    Atualizações dia 11/03/2024
    --Feito o carrossel de anúncios da pagina home;
    -- A FAZER: adicionar novos atributos nos anúncios no backend

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -3,8 +3,20 @@ import "./Home.css";
 // Components
 import SearchBar from "../../Components/SearchBar/SearchBar";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Pagination,
+  Navigation,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/bundle";
+
 // Hooks
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 // Redux
@@ -14,12 +26,32 @@ import AdsItem from "../../Components/Ads/AdsItem";
 const Home = () => {
   const dispatch = useDispatch();
 
+  const [slidePerView, setSlidePerView] = useState(3);
+
   const { ads } = useSelector((state) => state.ads);
 
   // Load all ads
   useEffect(() => {
     dispatch(getAds());
   }, [dispatch]);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 720) {
+        setSlidePerView(1);
+      } else {
+        setSlidePerView(3);
+      }
+    }
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div>
@@ -36,13 +68,45 @@ const Home = () => {
         <h2 id="carroussel-title">
           Conheça nossos imóveis à <span>venda</span>
         </h2>
-        <div className="carroussel">
-          {ads &&
-            ads.map((add) => (
-              <div key={add._id}>
-                <AdsItem add={add} />
-              </div>
-            ))}
+        <div className="carousel">
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+            slidesPerView={slidePerView}
+            pagination={{ clickable: true }}
+            navigation={true}
+          >
+            {ads &&
+              ads.map((add) => (
+                <SwiperSlide key={add._id}>
+                  <div key={add._id}>
+                    <AdsItem add={add} />
+                  </div>
+                </SwiperSlide>
+              ))}
+          </Swiper>
+        </div>
+      </div>
+
+      <div className="carroussel-ads">
+        <h2 id="carroussel-title">
+          Conheça nossos imóveis para <span>alugar</span>
+        </h2>
+        <div className="carousel">
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+            slidesPerView={slidePerView}
+            pagination={{ clickable: true }}
+            navigation={true}
+          >
+            {ads &&
+              ads.map((add) => (
+                <SwiperSlide key={add._id}>
+                  <div key={add._id}>
+                    <AdsItem add={add} />
+                  </div>
+                </SwiperSlide>
+              ))}
+          </Swiper>
         </div>
       </div>
     </div>
