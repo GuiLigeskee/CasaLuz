f71189de6ec18fc36bf4f367227304fe2b14e1a2
Guilherme Ligeski Saldanha <112131340+GuiLigeskee@users.noreply.github.com>
2024-04-18 16:05:44 -0300

    Atualizações dia 18/04/2024
    --Colocadas as opções para administradores;
    --A FAZER: Tela de atualizar o anúncio, e a funcionalidade de excluir anúncio;

--- frontend/src/Components/Ads/AdsItem.jsx
+++ frontend/src/Components/Ads/AdsItem.jsx
@@ -1,13 +1,47 @@
+import React from "react";
 import "./AdsItem.css";
 import { uploads } from "../../utils/config";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux"; // Importe o hook useSelector
 
 const AdsItem = ({ add }) => {
+  // Use o useSelector para acessar o estado global e obter informações sobre o usuário
+  const admin = useSelector((state) => state.auth.admin);
+
+  const handleUpdate = (e) => {
+    e.preventDefault(); // Evita o redirecionamento da página ao clicar no botão
+    // Lógica para atualizar o anúncio
+    alert("Atualizando anúncio...");
+  };
+
+  const handleDelete = (e) => {
+    e.preventDefault(); // Evita o redirecionamento da página ao clicar no botão
+    // Lógica para excluir o anúncio
+    alert("Excluindo anúncio...");
+  };
+
+  const renderAdminOptions = () => {
+    if (admin) {
+      return (
+        <div className="admin-options">
+          <button id="admin-option-update" onClick={handleUpdate}>
+            Atualizar
+          </button>
+          <button id="admin-option-delete" onClick={handleDelete}>
+            Excluir
+          </button>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
-    <Link to={`/ads/${add._id}`}>
-      <div className="ads-item">
-        {add.images && (
-          <div>
+    <div className="ads-item">
+      {add && (
+        <div>
+          {renderAdminOptions()}
+          <Link to={`/ads/${add._id}`}>
             <p className="price">R${add.price}</p>
             <img src={`${uploads}/ads/${add.images[0]}`} alt={add.title} />
             <p className="title">
@@ -17,10 +51,10 @@ const AdsItem = ({ add }) => {
               <br />
               {add.district}, {add.city}
             </p>
-          </div>
-        )}
-      </div>
-    </Link>
+          </Link>
+        </div>
+      )}
+    </div>
   );
 };
 
