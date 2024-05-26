fb85ec1244a9f499407d488ed16d168d6353aead
Eros Netto Antunes <erosnetto1002@gmail.com>
2024-05-26 12:00:21 -0300

    API ips

--- frontend/src/utils/config.jsx
+++ frontend/src/utils/config.jsx
@@ -1,7 +1,7 @@
 // export const api = "http://localhost:5000/api";
 // export const uploads = "http://localhost:5000/uploads";
-export const uploads = "http://77.37.69.144:81/uploads";
-export const api = "http://77.37.69.144:81/api";
+export const api = "https://apicasaluz.shop/api";
+export const uploads = "https://apicasaluz.shop/uploads";
 
 export const requestConfig = (method, data, token = null, image = null) => {
   let config;
